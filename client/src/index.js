5a1c09ff472c8c0194e04fd944cee3ce8facb8e8
Muhammad Kamran <mkamran1967@gmail.com>
2020-05-20 22:18:51 -0700

    Created map with geolocation

---
+++ client/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Imports
+import '../node_modules/leaflet/dist/leaflet.css'; // Importing leaflet css
+
+import App from './App';
+
+ReactDOM.render(<App />, document.getElementById('root'));
