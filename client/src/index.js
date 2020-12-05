5336b606b724b6400c351110fb817758daaf4fd3
Muhammad Kamran <mkamran1967@gmail.com>
2020-12-05 15:55:43 -0800

    TS Init

--- client/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-// Imports
-import '../node_modules/leaflet/dist/leaflet.css'; // Importing leaflet css
-import '../node_modules/bootstrap/dist/css/bootstrap.css';
-import App from './App';
-
-ReactDOM.render(<App />, document.getElementById('root'));
