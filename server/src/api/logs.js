93a7f0ad539e47b6f734e329c28e69cf3b80206b
Muhammad Kamran <mkamran1967@gmail.com>
2024-03-05 11:14:51 -0800

    Re-vamp start

--- server/src/api/logs.js
+++
@@ -1,8 +0,0 @@
-const { Router } = require('express');
-const { getLogs, postLogs } = require('../controllers/logControllers');
-
-const router = Router();
-
-router.route('/').get(getLogs).post(postLogs);
-
-module.exports = router;
